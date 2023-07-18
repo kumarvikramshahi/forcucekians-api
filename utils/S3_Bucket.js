cbfa947c825e982031e75363054c4db615e09c1d
kumarvikramshahi <63746254+kumarvikramshahi@users.noreply.github.com>
2023-07-18 12:08:20 +0530

    updating env vars

--- utils/S3_Bucket.js
+++ utils/S3_Bucket.js
@@ -4,7 +4,7 @@ const S3 = require('aws-sdk/clients/s3');
 
 const bucketName = process.env.AWS_S3_BUCKET
 const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const accessKeyId = process.env.MY_AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY_ID
 
 const s3 = new S3({
